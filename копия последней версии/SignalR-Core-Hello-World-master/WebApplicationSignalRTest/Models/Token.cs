3b72c9453e1b98b3b7951bc18d2b41b2ffe4817b
Maxim <89503964596@mail.ru>
2019-03-11 18:13:30 +0800

    Работает регистрация и авторизация на HTML

---
+++ копия последней версии/SignalR-Core-Hello-World-master/WebApplicationSignalRTest/Models/Token.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerNetCore.Models
+{
+    public class Token
+    {
+        public int TokenId { get; set; }
+        public Guid Guid { get; set; }
+        public bool IsActive { get; set; }
+        public string CompName { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
